963928290ea3d6c8f65a402de4c47d6b06a96a5b
caf3babe <starv0411@gmail.com>
2022-01-05 18:08:49 +0100

    refactored deprecated class, removed unused methods

--- src/main/java/at/ac/fhcampuswien/usermanager/ui/controller/SaveButtonController.java
+++ src/main/java/at/ac/fhcampuswien/usermanager/ui/controller/SaveButtonController.java
@@ -1,13 +1,10 @@
 package at.ac.fhcampuswien.usermanager.ui.controller;
 
 import at.ac.fhcampuswien.usermanager.ui.NewPasswordScreen;
-import at.ac.fhcampuswien.usermanager.ui.Password;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class SaveButtonController implements ActionListener {
     private final NewPasswordScreen newPassword;
@@ -27,24 +24,5 @@ public class SaveButtonController implements ActionListener {
             JOptionPane.showMessageDialog(this.newPassword.getFrame(), ex.getMessage());
         }
 
-    }
-
-    private void oldMethod() {
-        if (this.newPassword.getPasswordField().getPassword().length== 0){
-            JOptionPane.showMessageDialog(null, "Please fill out every field.");
-        }else {
-            char[] password1 = this.newPassword.getPasswordField().getPassword();
-            char[] password2 = this.newPassword.getPasswordRepeatField().getPassword();
-
-            if (Arrays.equals(password1, password2)) {
-                //ArrayList student = (ArrayList) this.newPassword.getUsers().get(0);
-                char[] savePassword = Password.verschluesseln(5, password1);
-                String str = new String(savePassword);
-                //student.set(3, str);
-                this.newPassword.getFrame().dispose();
-            }else {
-                JOptionPane.showMessageDialog(null, "Passwords are not equal. Please redo.");
-            }
-        }
     }
 }
